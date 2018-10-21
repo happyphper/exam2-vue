c037b97d87b8d5c363e09c48ade647e0a1351a24
Baron <282584778@qq.com>
2018-10-21 23:54:15 +0800

    移除默认账户密码

--- src/views/auth/index.vue
+++ src/views/auth/index.vue
@@ -44,8 +44,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin@admin.com',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur' }],
